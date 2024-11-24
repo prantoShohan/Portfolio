746df03fdb0a7e5df0e3ac0833e32a71da7a0dbf
eftakher.sazid <eftakher.sazid@grameenphone.com>
2024-11-24 13:31:01 +0600

    Working on emergence

--- src/features/emergence/index.tsx
+++ src/features/emergence/index.tsx
@@ -5,105 +5,76 @@ import React from 'react';
 
 const EmergenceDetails = () => {
   return (
-    <div className="bg-white min-h-screen">
-      {/* Emergence Details */}
-      <div className="relative h-[460px]">
-        <Image
-          src={'/images/img_1.jpg'}
-          className="object-cover"
-          alt=""
-          fill
-          unoptimized
-        />
-        <div className="absolute bottom-0 responsive-padding w-full bg-black/50">
-          <div className="w-full px-4 pb-4 backdrop:blur space-y-2 ">
-            <div className="text-5xl font-bold ">{`Emergence`}</div>
-            <div className="text-base font-bold top-40 ">{`Computational Growth of Urban Organism`}</div>
-          </div>
-        </div>
-      </div>
-      <div className="responsive-padding text-black py-4 space-y-7">
-        <div id="growth" className="flex justify-between space-x-3">
-          <div id="text" className="max-w-[40%] space-y-2">
-            <div className="text-3xl font-bold">
-              The growth of Dhaka in last 30 years.
-            </div>
-            <div>
-              Dhaka is one of the most densly populated cities in the world. It
-              has rapid growth in last 30 years with steady decline in its
-              wetland and natural landscapes. It has become an parasite. This
-              project poses an alternative means of growth through simulating
-              natural growth process.
-            </div>
-            <div id="multimedia" className="relative h-[180px]">
-              <Image
-                src={'/images/emergence/graph.png'}
-                className="object-contain"
-                alt=""
-                fill
-                unoptimized
-              />
-            </div>
-          </div>
-          <div id="multimedia" className="relative h-[400px] w-[60%]">
-            <Image
-              src={'/images/emergence/dhaka timelapse small.gif'}
-              className="object-cover"
-              alt=""
-              fill
-              unoptimized
-            />
-          </div>
-        </div>
-        <div id="growth_2" className="flex justify-between space-x-3">
-          {/* <div
-            id="multimedia"
-            className="relative grid grid-cols-3 h-[400px] w-[40%]"
-          >
-            <Image
-              src={'/images/emergence/dhaka timelapse small.gif'}
-              className="object-cover"
-              alt=""
-              fill
-              unoptimized
-            />
-            <Image
-              src={'/images/emergence/dhaka timelapse small.gif'}
-              className="object-cover"
-              alt=""
-              fill
-              unoptimized
-            />
-            <Image
-              src={'/images/emergence/dhaka timelapse small.gif'}
-              className="object-cover"
-              alt=""
-              fill
-              unoptimized
-            />
-            <Image
-              src={'/images/emergence/dhaka timelapse small.gif'}
-              className="object-cover"
-              alt=""
-              fill
-              unoptimized
-            />
-          </div> */}
-          <div id="text" className="max-w-[60%] space-y-2">
-            <div className="text-3xl font-bold">
-              The growth of Dhaka in last 30 years.
-            </div>
-            <div>
-              Dhaka is one of the most densly populated cities in the world. It
-              has rapid growth in last 30 years with steady decline in its
-              wetland and natural landscapes. It has become an parasite. This
-              project poses an alternative means of growth through simulating
-              natural growth process.
+
+    
+    <div className = "bg-white min-h-screen relative">
+        <div className="relative h-[460px]">
+        {/* Cover Image */}
+          <Image
+            src={'/images/emergence/Slime full.jpg'}
+            className="object-cover"
+            alt=""
+            fill
+            unoptimized
+          />
+          <div className="absolute bottom-0  w-full bg-black/50 responsive-padding">
+            <div className="w-full pb-4 backdrop:blur space-y-2 ">
+              {/* Title */}
+              <div className="text-5xl font-bold">Emergence</div>
+              {/* Subtitle */}
+              <div className="text-base font-bold top-40 ">
+                Computational Growth of Urban Organism
+              </div>
             </div>
           </div>
         </div>
-      </div>
+
+
+
+        <div className="text-black relative px-4 responsive-padding ">
+
+          {/* {custom sections } */}
+
+
+          <div className=" text-black py-10 relative space-x-3  border">
+            <div className="space-y-7 section-text ">
+
+                <div
+                  className="flex flex-row space-x-10 relative text-section  border"
+                  id="Urban Organismr"
+                  //ref={tenStepSequencerRef}
+                >
+                  <div className="section-text w-[20%] border basis-1/3">
+                    <div className="section-title border">
+                      Urban Organism
+                    </div>
+                    Dhaka is one of the most densly populated cities in the
+                    world. It has rapid growth in last 30 years with steady
+                    decline in its wetland and natural landscapes. It has
+                    become an parasite. This project poses an alternative
+                    means of growth through simulating natural growth process.
+                  </div>
+                  <div id="multimedia" className="relative w-[60%] h-[460px] border basis-2/3">
+                    <Image
+                      src={'/images/emergence/dhaka timelapse small.gif'}
+                      className="object-cover"
+                      alt=""
+                      fill
+                      unoptimized
+                    />
+                  </div>
+                </div>
+              
+
+            </div>
+          </div>
+        </div>
     </div>
+
+
+
+
+
   );
 };
 
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="!min-h-[10vh] flex items-center border border-white/5 bg-background w-full responsive-padding z-30 sticky top-0">
+    <div className="!min-h-[4vh] flex items-center border border-white/5 bg-background w-full responsive-padding z-30 sticky top-0">
       <div className="">
-        <Link href={'/'} className="text-5xl font-bold">
+        <Link href={'/'} className="text-2xl font-bold">
           Pranto Shohan
         </Link>
       </div>
