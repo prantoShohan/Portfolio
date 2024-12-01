6330f39b22bd9965d65a2791e417067bbcb835ff
prantoShohan <me.pranto@gmail.com>
2024-12-01 12:37:23 +0000

    resize canvas

--- src/features/landing/heroSection.tsx
+++ src/features/landing/heroSection.tsx
@@ -316,7 +316,7 @@ const sketch: Sketch = (p5: p5) => {
   };
 
   p5.windowResized = () => {
-    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    // p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
     p5.setup()
   };
 };
