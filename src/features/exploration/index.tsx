2bb77044c0f83ca5837ab0ee8f0caa687d2834f6
prantoShohan <me.pranto@gmail.com>
2024-12-05 14:42:37 +0000

    updated exploration

--- src/features/exploration/index.tsx
+++ src/features/exploration/index.tsx
@@ -8,10 +8,14 @@ import { SectionType } from '@/model/sectiontype';
 
 const Exploration = () => {
   const overviewRef = useRef(null);
+  const MetamorphosisRef = useRef(null);
+  const DeconstructionRef = useRef(null);
+  const PathRef = useRef(null);
+  const ModulesRef = useRef(null);
 
 
   const sectionRefs = [
-    overviewRef, 
+    overviewRef, MetamorphosisRef, DeconstructionRef, PathRef, ModulesRef
 
   ];
 
@@ -21,6 +25,26 @@ const Exploration = () => {
     {
       title: 'Overview',
       ref: overviewRef,
+      subsections: [],
+    },
+    {
+      title: 'Metamorphosis of Surface',
+      ref: MetamorphosisRef,
+      subsections: [],
+    },
+    {
+      title: 'Deconstruction of Form',
+      ref: DeconstructionRef,
+      subsections: [],
+    },
+    {
+      title: 'Creation of Path',
+      ref: PathRef,
+      subsections: [],
+    },
+    {
+      title: 'Generation of Form',
+      ref: ModulesRef,
       subsections: [],
     },
     
@@ -53,7 +77,7 @@ const Exploration = () => {
 
 
   return (
-    <div className = "bg-white min-h-screen relative">
+    <div className = "bg-white min-h-screen relative ">
       {/* Cover */}
       <div className="relative h-[460px] bg-white">
         <Image
@@ -63,8 +87,8 @@ const Exploration = () => {
           fill
           unoptimized
         />
-        <div className="absolute bottom-0 backdrop-blur-sm w-full bg-black/30 responsive-padding">
-          <div className="w-full pb-4  space-y-2">
+        <div className="absolute bottom-0 backdrop-blur-sm w-full bg-black/30 ">
+          <div className="w-full pb-4  space-y-2 responsive-padding">
             <div className="text-5xl font-bold ">Exploration</div>
             <div className="text-base font-bold top-40 ">
                 of Parametric Forms
@@ -75,76 +99,106 @@ const Exploration = () => {
 
 
 
-      <div className="text-black relative px-4 ">
+      <div className="text-black relative px-4 responsive-padding-pages">
+        <CustomSections
+              sections={EMERGENCE_SECTIONS}
+              activeSection={activeSection}
+            />
       
         
-        <div
+        <div>
+          <div className="section-title" 
           id="Overview"
-          ref={overviewRef}>
-          <div className="section-title">Overview</div>
-          <div className="section-text">
+          ref={overviewRef}>Overview</div>
+          <div className="section-text max-w-[600px]">
             These projects were done during the first year of my architecture school.
              Having prior experience in programming, I became immidiately interested in Grasshopper.
-             As I experimented with grasshopper for my class project, it became intuitive to me.
+             As I experimented with grasshopper for my class projects, it became intuitive to me.
 
           </div>
 
-          <div className="w-full mt-4">
-            <img 
-              src="/images/exploration/10.jpeg"
-              className="w-full h-auto object-contain"
-              alt="Reaction Diffusion"
-            />
+          <div>
+            <div className="section-title "
+              id="Metamorphosis of Surface"
+              ref={MetamorphosisRef}>Metamorphosis of Surface</div>
+              <div className="w-full mt-4">
+              <img 
+                src="/images/exploration/10.jpeg"
+                className="w-full h-auto object-contain"
+                alt="Reaction Diffusion"
+              />
+            </div>
+            <div className="w-full mt-4">
+              <img 
+                src="/images/exploration/20.jpeg"
+                className="w-full h-auto object-contain"
+                alt="Reaction Diffusion"
+              />
+            </div>
           </div>
-          <div className="w-full mt-4">
-            <img 
-              src="/images/exploration/20.jpeg"
-              className="w-full h-auto object-contain"
-              alt="Reaction Diffusion"
-            />
+
+          <div>
+            <div className="section-title " 
+            id="Deconstruction of Form"
+            ref={DeconstructionRef}>Deconstruction of Form</div>
+              <div className="w-full mt-4">
+              <img 
+                src="/images/exploration/30.jpeg"
+                className="w-full h-auto object-contain"
+                alt="Reaction Diffusion"
+              />
+            </div>
+            <div className="w-full mt-4">
+              <img 
+                src="/images/exploration/40.jpeg"
+                className="w-full h-auto object-contain"
+                alt="Reaction Diffusion"
+              />
+            </div>
           </div>
-          <div className="w-full mt-4">
-            <img 
-              src="/images/exploration/30.jpeg"
-              className="w-full h-auto object-contain"
-              alt="Reaction Diffusion"
-            />
+          
+          <div>
+            <div className="section-title" 
+              id="Creation of Path"
+              ref={PathRef}>Creation of Path</div>
+              <div className="w-full mt-4">
+              <img 
+                src="/images/exploration/50.jpeg"
+                className="w-full h-auto object-contain"
+                alt="Reaction Diffusion"
+              />
+            </div>
+            <div className="w-full mt-4">
+              <img 
+                src="/images/exploration/60.jpeg"
+                className="w-full h-auto object-contain"
+                alt="Reaction Diffusion"
+              />
+            </div>
           </div>
-          <div className="w-full mt-4">
-            <img 
-              src="/images/exploration/40.jpeg"
-              className="w-full h-auto object-contain"
-              alt="Reaction Diffusion"
-            />
+
+          <div>
+            <div className="section-title "
+            id="Generation of Form"
+            ref={ModulesRef}>Generation of Form</div>
+              <div className="w-full mt-4">
+              <img 
+                src="/images/exploration/70.jpeg"
+                className="w-full h-auto object-contain"
+                alt="Reaction Diffusion"
+              />
+            </div>
+            <div className="w-full mt-4">
+              <img 
+                src="/images/exploration/80.jpeg"
+                className="w-full h-auto object-contain"
+                alt="Reaction Diffusion"
+              />
+            </div>
           </div>
-          <div className="w-full mt-4">
-            <img 
-              src="/images/exploration/50.jpeg"
-              className="w-full h-auto object-contain"
-              alt="Reaction Diffusion"
-            />
-          </div>
-          <div className="w-full mt-4">
-            <img 
-              src="/images/exploration/60.jpeg"
-              className="w-full h-auto object-contain"
-              alt="Reaction Diffusion"
-            />
-          </div>
-          <div className="w-full mt-4">
-            <img 
-              src="/images/exploration/70.jpeg"
-              className="w-full h-auto object-contain"
-              alt="Reaction Diffusion"
-            />
-          </div>
-          <div className="w-full mt-4">
-            <img 
-              src="/images/exploration/80.jpeg"
-              className="w-full h-auto object-contain"
-              alt="Reaction Diffusion"
-            />
-          </div>
+
+
+          
 
         </div>
 
