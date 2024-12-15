dd58c5f378195016e0f1b269cd865716d6bc8dd3
eftakher.sazid <eftakher.sazid@grameenphone.com>
2024-12-15 20:31:30 +0600

    Added rest of the stuffs

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div className="h-[65vh]">Loading</div>;
+};
+
+export default index;
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,12 +1,18 @@
 'use client';
 
+import { cn } from '@/lib/utils';
+import Hamburger from 'hamburger-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
+import React, { useState } from 'react';
 
 const Header = () => {
   const path = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
 
   return (
     <div className="!min-h-[4vh] flex items-center  bg-[#222222] w-full responsive-padding z-30 sticky top-0 justify-between">
@@ -15,25 +21,41 @@ const Header = () => {
           Pranto Shohan
         </Link>
       </div>
-      {path === '/' ? (
-        <div className="space-x-3">
-          <AnchorLink className="link-hover" href="#projects">
-            Projects
-          </AnchorLink>
-          <AnchorLink className="link-hover" href="#about-me">
-            About me
-          </AnchorLink>
-        </div>
-      ) : (
-        <div className="space-x-3">
-          <Link className="link-hover" href="/#projects">
-            Projects
-          </Link>
-          <Link className="link-hover" href="/#about-me">
-            About me
-          </Link>
-        </div>
-      )}
+
+      <div className="space-x-3">
+        <Hamburger onToggle={toggleMenu} toggled={isMenuOpen} size={20} />
+        {isMenuOpen && (
+          <div
+            className={cn(
+              'absolute animate-in fade-in-0 zoom-in-95 duration-600 flex flex-col space-y-4 shadow-md rounded-md md:right-[26%] right-[2%] -left-[2%] md:left-auto z-50 px-4 py-4 md:w-[200px] bg-black/60',
+              isMenuOpen
+                ? 'opacity-100 translate-y-0'
+                : 'opacity-0 -translate-y-4'
+            )}
+          >
+            <div className="pt-4">
+              <Link
+                href={'/#projects'}
+                className="link-hover font-medium"
+                onClick={toggleMenu}
+              >
+                <div className="flex items-center space-x-1">
+                  <span>Projects</span>
+                </div>
+              </Link>
+              <Link
+                href={'/#projects'}
+                className="link-hover font-medium"
+                onClick={toggleMenu}
+              >
+                <div className="flex items-center space-x-1">
+                  <span>About me</span>
+                </div>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
