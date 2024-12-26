890cfa9e2b2b5234d4bed4e33e7b23417afc8e0b
prantoShohan <me.pranto@gmail.com>
2024-12-26 09:17:51 +0600

    analytics

--- src/layout/siteLayout.tsx
+++ src/layout/siteLayout.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 import { usePathname } from 'next/navigation';  // Import usePathname
+import { Analytics } from '@vercel/analytics/next';
 
 const SiteLayout = ({ children }: { readonly children: React.ReactNode }) => {
   const pathname = usePathname();
@@ -17,6 +18,7 @@ const SiteLayout = ({ children }: { readonly children: React.ReactNode }) => {
       {showHeader && <Header />}
       <div className="min-h-[84vh] h-full w-full pb- relative ">
         {children}
+        <Analytics />
       </div>
       <Footer />
     </div>
